634964e55ba0b549f3ccc99fa10f813ca83c5949
lmachens <leon@machens.koeln>
2021-10-17 08:28:42 +0200

    🚑 Fix enemey area crash

--- src/app/components/WorldMap/useLayerGroups.ts
+++ src/app/components/WorldMap/useLayerGroups.ts
@@ -41,90 +41,96 @@ function useLayerGroups({
     const removableMarkers = Object.keys(allLayers);
 
     for (let i = 0; i < visibleMarkers.length; i++) {
-      const marker = visibleMarkers[i];
-      if (allLayers[marker._id]) {
-        const index = removableMarkers.indexOf(marker._id);
-        if (index > -1) {
-          removableMarkers.splice(index, 1);
+      try {
+        const marker = visibleMarkers[i];
+        if (allLayers[marker._id]) {
+          const index = removableMarkers.indexOf(marker._id);
+          if (index > -1) {
+            removableMarkers.splice(index, 1);
+          }
+          if (allLayers[marker._id].hasComments !== Boolean(marker.comments)) {
+            allLayers[marker._id].layer.removeFrom(leafletMap);
+            delete allLayers[marker._id];
+          } else {
+            continue;
+          }
         }
-        if (allLayers[marker._id].hasComments !== Boolean(marker.comments)) {
-          allLayers[marker._id].layer.removeFrom(leafletMap);
-          delete allLayers[marker._id];
-        } else {
+        const mapFilter = mapFilters.find(
+          (mapFilter) => mapFilter.type === marker.type
+        );
+        if (!mapFilter) {
           continue;
         }
-      }
-      const mapFilter = mapFilters.find(
-        (mapFilter) => mapFilter.type === marker.type
-      );
-      if (!mapFilter) {
-        continue;
-      }
 
-      if (marker.position) {
-        const mapMarker = new CanvasMarker(
-          [marker.position[1], marker.position[0]],
-          {
-            radius: 16,
-            image: {
-              src: mapFilter.iconUrl,
-              size: [40, 40],
-              comments: marker.comments,
-            },
-            pmIgnore: true,
-          }
-        ).bindTooltip(getTooltipContent(marker, mapFilter), {
-          direction: 'top',
-        });
-        if (onMarkerClick) {
-          mapMarker.on('click', () => {
-            onMarkerClick(marker);
+        if (marker.position) {
+          const mapMarker = new CanvasMarker(
+            [marker.position[1], marker.position[0]],
+            {
+              radius: 16,
+              image: {
+                src: mapFilter.iconUrl,
+                size: [40, 40],
+                comments: marker.comments,
+              },
+              pmIgnore: true,
+            }
+          ).bindTooltip(getTooltipContent(marker, mapFilter), {
+            direction: 'top',
           });
-        }
-        allLayers[marker._id] = {
-          layer: mapMarker,
-          hasComments: Boolean(marker.comments),
-        };
-      } else if (marker.positions) {
-        const layerGroup = new leaflet.LayerGroup();
+          if (onMarkerClick) {
+            mapMarker.on('click', () => {
+              onMarkerClick(marker);
+            });
+          }
+          allLayers[marker._id] = {
+            layer: mapMarker,
+            hasComments: Boolean(marker.comments),
+          };
+          allLayers[marker._id].layer.addTo(leafletMap);
+        } else if (marker.positions) {
+          const layerGroup = new leaflet.LayerGroup();
 
-        const polygon = leaflet.polygon(
-          marker.positions.map((position) => [position[1], position[0]])
-        );
+          const polygon = leaflet.polygon(
+            marker.positions.map((position) => [position[1], position[0]])
+          );
 
-        layerGroup.addLayer(polygon);
-        const text = leaflet.divIcon({
-          className: classNames(
-            'leaflet-polygon-text',
-            `leaflet-polygon-text-${leafletMap.getZoom()}`
-          ),
-          html: `${marker.name}<br/>(${marker.levelRange?.join('-')})`,
-        });
-        const textMarker = leaflet.marker(polygon.getCenter(), {
-          icon: text,
-        });
-
-        leafletMap.on('zoomend', () => {
-          const element = textMarker.getElement();
-          if (element) {
-            element.className = classNames(
+          layerGroup.addLayer(polygon);
+          allLayers[marker._id] = {
+            layer: layerGroup,
+            hasComments: Boolean(marker.comments),
+          };
+          allLayers[marker._id].layer.addTo(leafletMap);
+          const text = leaflet.divIcon({
+            className: classNames(
               'leaflet-polygon-text',
               `leaflet-polygon-text-${leafletMap.getZoom()}`
-            );
-          }
-        });
-        layerGroup.addLayer(textMarker);
-        allLayers[marker._id] = {
-          layer: layerGroup,
-          hasComments: Boolean(marker.comments),
-        };
-        if (onMarkerClick) {
-          polygon.on('click', () => {
-            onMarkerClick(marker);
+            ),
+            html: `${marker.name}<br/>(${marker.levelRange?.join('-')})`,
           });
+          const textMarker = leaflet.marker(polygon.getCenter(), {
+            icon: text,
+          });
+
+          leafletMap.on('zoomend', () => {
+            const element = textMarker.getElement();
+            if (element) {
+              element.className = classNames(
+                'leaflet-polygon-text',
+                `leaflet-polygon-text-${leafletMap.getZoom()}`
+              );
+            }
+          });
+          layerGroup.addLayer(textMarker);
+
+          if (onMarkerClick) {
+            polygon.on('click', () => {
+              onMarkerClick(marker);
+            });
+          }
         }
+      } catch (error) {
+        console.error(error);
       }
-      allLayers[marker._id].layer.addTo(leafletMap);
     }
 
     removableMarkers.forEach((markerId) => {
