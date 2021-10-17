4733e77eaee8e00ae2e12af19d1e03465a03773c
lmachens <leon@machens.koeln>
2021-10-17 16:59:11 +0200

    ✨ Remember marker settings in local storage

--- src/app/components/Settings/Settings.tsx
+++ src/app/components/Settings/Settings.tsx
@@ -1,17 +1,34 @@
+import { usePersistentState } from '../../utils/storage';
 import styles from './Settings.module.css';
 
 function Settings(): JSX.Element {
+  const [markerSize, setMarkerSize] = usePersistentState('markerSize', 40);
+  const [markerBackground, setMarkerBackground] = usePersistentState(
+    'markerBackground',
+    false
+  );
+
   return (
     <div className={styles.container}>
       <h2>Settings</h2>
       <h3>Markers</h3>
       <label className={styles.label}>
         Marker size
-        <input type="range" />
+        <input
+          type="range"
+          value={markerSize}
+          onChange={(event) => setMarkerSize(+event.target.value)}
+          min={10}
+          max={100}
+        />
       </label>
       <label className={styles.label}>
         Show background
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={markerBackground}
+          onChange={(event) => setMarkerBackground(event.target.checked)}
+        />
       </label>
     </div>
   );
