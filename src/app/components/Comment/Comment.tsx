9eb46ea439d6d146716d269a33b3ac657f96977e
lmachens <leon@machens.koeln>
2021-10-19 10:06:02 +0200

    ✨ Allow removing comments Fixes #63

--- src/app/components/Comment/Comment.tsx
+++ src/app/components/Comment/Comment.tsx
@@ -1,14 +1,45 @@
 import { toTimeAgo } from '../../utils/dates';
 import styles from './Comment.module.css';
 import Markdown from 'markdown-to-jsx';
+import DeleteIcon from '../icons/DeleteIcon';
+import { fetchJSON } from '../../utils/api';
 
 type CommentProps = {
+  id: string;
+  userId?: string;
   username: string;
   createdAt: Date;
   message: string;
+  removable: boolean;
+  onRemove: () => void;
 };
 
-function Comment({ username, createdAt, message }: CommentProps): JSX.Element {
+function Comment({
+  id,
+  userId,
+  username,
+  createdAt,
+  message,
+  removable,
+  onRemove,
+}: CommentProps): JSX.Element {
+  async function handleRemove(): Promise<void> {
+    try {
+      await fetchJSON(`/api/comments/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: userId,
+        }),
+      });
+      onRemove();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <article className={styles.container}>
       <p className={styles.name}>{username}</p>
@@ -16,6 +47,11 @@ function Comment({ username, createdAt, message }: CommentProps): JSX.Element {
       <p className={styles.message}>
         <Markdown>{message}</Markdown>
       </p>
+      {removable && (
+        <button className={styles.remove} onClick={handleRemove}>
+          <DeleteIcon />
+        </button>
+      )}
     </article>
   );
 }
