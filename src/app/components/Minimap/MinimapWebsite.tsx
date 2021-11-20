a6d3d7263ae309d812cd92178fe240f7e098d282
lmachens <leon@machens.koeln>
2021-11-20 17:44:27 +0100

    ✨ Add minimap setup

--- src/app/components/Minimap/MinimapWebsite.tsx
+++ src/app/components/Minimap/MinimapWebsite.tsx
@@ -1,4 +1,21 @@
+import { usePersistentState } from '../../utils/storage';
+import styles from './MinimapSetup.module.css';
+
 function MinimapWebsite(): JSX.Element {
+  const [minimapOpacity, setMinimapOpacity] = usePersistentState(
+    'minimapOpacity',
+    80
+  );
+  const [minimapBorderRadius, setMinimapBorderRadius] = usePersistentState(
+    'minimapBorderRadius',
+    50
+  );
+  const [minimapZoom, setMinimapZoom] = usePersistentState('minimapZoom', 5);
+  const [rotateMinimap, setRotateMinimap] = usePersistentState(
+    'rotateMinimap',
+    false
+  );
+
   return (
     <>
       <p>
@@ -16,6 +33,55 @@ function MinimapWebsite(): JSX.Element {
         </a>
         .
       </p>
+      <h5>Settings</h5>
+      <section className={styles.settings}>
+        <label>
+          URL
+          <input disabled value={`${location.origin}/minimap.html`} />
+        </label>
+        <label>
+          Zoom
+          <input
+            type="range"
+            value={minimapZoom}
+            min={0}
+            max={6}
+            onMouseDown={(event) => event.stopPropagation()}
+            onChange={(event) => setMinimapZoom(+event.target.value)}
+          />
+        </label>
+        <label>
+          Border
+          <input
+            type="range"
+            value={minimapBorderRadius}
+            min={0}
+            max={50}
+            onMouseDown={(event) => event.stopPropagation()}
+            onChange={(event) => setMinimapBorderRadius(+event.target.value)}
+          />
+        </label>
+        <label>
+          Opacity
+          <input
+            type="range"
+            value={minimapOpacity}
+            min={20}
+            max={100}
+            onMouseDown={(event) => event.stopPropagation()}
+            onChange={(event) => setMinimapOpacity(+event.target.value)}
+          />
+        </label>
+        <label>
+          Rotate minimap
+          <input
+            type="checkbox"
+            checked={rotateMinimap}
+            onMouseDown={(event) => event.stopPropagation()}
+            onChange={(event) => setRotateMinimap(event.target.checked)}
+          />
+        </label>
+      </section>
     </>
   );
 }
