6403913d80bde2ec93542a88856612a3859c0e24
lmachens <leon@machens.koeln>
2021-11-24 11:59:09 +0100

    🐛 Copy to clipboard in Overwolf

--- src/app/utils/clipboard.ts
+++ src/app/utils/clipboard.ts
@@ -1,7 +1,14 @@
 import { notify } from './notifications';
+import { isOverwolfApp } from './overwolf';
 
 export const copyTextToClipboard = (text: string): Promise<void> => {
-  return notify(navigator.clipboard.writeText(text), {
+  const action = isOverwolfApp
+    ? new Promise<void>((resolve) => {
+        resolve(overwolf.utils.placeOnClipboard(text));
+      })
+    : navigator.clipboard.writeText(text);
+
+  return notify(action, {
     success: 'Copied to clipboard 📝',
   });
 };
