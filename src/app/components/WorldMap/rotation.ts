be41f1046c4f13b88ea937ffdbf183806eece43d
lmachens <leon@machens.koeln>
2021-11-26 11:13:33 +0100

    🐛 Markers are not visible on minimap on rotation Fixes #204

--- src/app/components/WorldMap/rotation.ts
+++ src/app/components/WorldMap/rotation.ts
@@ -4,8 +4,6 @@ export function updateRotation(markers: CanvasMarker[], rotate: number) {
   if (markers.length === 0) {
     return;
   }
-  // @ts-ignore
-  markers[0]._renderer._clear();
   markers.forEach((marker) => {
     marker.options.image.rotate = rotate;
     marker.redraw();
