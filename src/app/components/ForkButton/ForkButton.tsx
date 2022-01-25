4c8245f405e9323d84f7c8955fe220938d5ec9eb
lmachens <leon@machens.koeln>
2022-01-25 18:30:45 +0100

    🚨 Fix invalid property name

--- src/app/components/ForkButton/ForkButton.tsx
+++ src/app/components/ForkButton/ForkButton.tsx
@@ -32,17 +32,17 @@ function ForkButton({
     >
       <svg width="1em" height="1em" viewBox="0 0 24 24">
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M12 21a1.75 1.75 0 1 1 0-3.5a1.75 1.75 0 0 1 0 3.5zm-3.25-1.75a3.25 3.25 0 1 0 6.5 0a3.25 3.25 0 0 0-6.5 0zm-3-12.75a1.75 1.75 0 1 1 0-3.5a1.75 1.75 0 0 1 0 3.5zM2.5 4.75a3.25 3.25 0 1 0 6.5 0a3.25 3.25 0 0 0-6.5 0zM18.25 6.5a1.75 1.75 0 1 1 0-3.5a1.75 1.75 0 0 1 0 3.5zM15 4.75a3.25 3.25 0 1 0 6.5 0a3.25 3.25 0 0 0-6.5 0z"
           fill="currentColor"
         />
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M6.5 7.75v1A2.25 2.25 0 0 0 8.75 11h6.5a2.25 2.25 0 0 0 2.25-2.25v-1H19v1a3.75 3.75 0 0 1-3.75 3.75h-6.5A3.75 3.75 0 0 1 5 8.75v-1h1.5z"
           fill="currentColor"
         />
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M11.25 16.25v-5h1.5v5h-1.5z"
           fill="currentColor"
         />
