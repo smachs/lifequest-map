e02f0d349a6e384fc6fd2463d029dec071813eac
lmachens <leon@machens.koeln>
2022-08-13 20:18:45 +0200

    ✨ Add links to my other projects

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,16 +1,49 @@
-import { useAccount } from '../../contexts/UserContext';
 import styles from './Footer.module.css';
 
 function Footer() {
-  const { account } = useAccount();
-
   return (
     <footer className={styles.footer}>
       <small>
-        {account?.hideAds
-          ? 'No ads for you, thx for your support 🤘'
-          : 'No Cross-Site trackers and other creepy things'}{' '}
-        |{' '}
+        <a
+          href="https://www.overwolf.com/app/Leon_Machens-Aeternum_Map"
+          title="Aeternum Map Companion"
+          target="_blank"
+        >
+          Overwolf App
+        </a>
+        |
+        <a
+          href="https://www.arkesia.gg/"
+          title="Interactive map for Lost Ark"
+          target="_blank"
+        >
+          Arkesia.gg
+        </a>
+        |
+        <a
+          href="https://th.gl/"
+          title="Trophies app for League of Legends"
+          target="_blank"
+        >
+          Trophy Hunter
+        </a>
+        |
+        <a
+          href="https://www.soc.gg/"
+          title="A Songs of Conquest fansite"
+          target="_blank"
+        >
+          SoC.gg
+        </a>
+        |
+        <a
+          href="https://github.com/lmachens/skeleton"
+          title="Simply display any website as customizable Overlay"
+          target="_blank"
+        >
+          Skeleton
+        </a>
+        |
         <a href="/privacy.html" target="_blank">
           Privacy Policy
         </a>
