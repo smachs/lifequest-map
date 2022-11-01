b49567480968fe9e2ef28d88a5ff6d099b89eb6e
lmachens <leon@machens.koeln>
2022-11-01 10:11:14 +0100

    Fix SSR of experimental map

--- apps/web/app/Map.tsx
+++ apps/web/app/Map.tsx
@@ -1,9 +1,6 @@
 'use client';
-
-import type { TileLayer } from 'leaflet';
-import leaflet from 'leaflet';
+import type { TileLayer, Map as LeaftletMap } from 'leaflet';
 import { useEffect, useMemo, useRef } from 'react';
-import 'tilelayer-canvas';
 import { AETERNUM_MAP } from 'static';
 import useSWR from 'swr';
 
@@ -17,34 +14,7 @@ function toThreeDigits(number: number): string {
   return `${number}`;
 }
 
-const worldCRS = leaflet.extend({}, leaflet.CRS.Simple, {
-  transformation: new leaflet.Transformation(1 / 16, 0, -1 / 16, 0),
-});
-
-const WorldTiles = (map: string): new () => TileLayer =>
-  // @ts-ignore
-  leaflet.TileLayer.Canvas.extend({
-    // options: {
-    //   errorTileUrl: `${VITE_API_ENDPOINT}/assets/map/empty.webp`,
-    // },
-    getTileUrl(coords: { x: number; y: number; z: number }) {
-      const zoom = 8 - coords.z - 1;
-      const multiplicators = [1, 2, 4, 8, 16, 32, 64];
-      const x = coords.x * multiplicators[zoom - 1];
-      const y = (-coords.y - 1) * multiplicators[zoom - 1];
-      if (x < 0 || y < 0 || y >= 64 || x >= 64) {
-        return `https://aeternum-map.gg/assets/map/empty.webp`;
-      }
-      return `https://aeternum-map.gg/assets/${map}/map_l${zoom}_y${toThreeDigits(
-        y
-      )}_x${toThreeDigits(x)}.webp`;
-    },
-    getTileSize() {
-      return { x: 1024, y: 1024 };
-    },
-  });
-
-export let leafletMap: leaflet.Map | null = null;
+export let leafletMap: LeaftletMap | null = null;
 
 type LiveCharacter = {
   position: {
@@ -79,31 +49,63 @@ export default function Map() {
     if (!mapElement || leafletMap || !data) {
       return;
     }
-    const latLngBounds = leaflet.latLngBounds(AETERNUM_MAP.maxBounds);
-    leafletMap = leaflet.map(mapElement, {
-      preferCanvas: true,
-      crs: worldCRS,
-      maxZoom: AETERNUM_MAP.maxZoom,
-      minZoom: AETERNUM_MAP.minZoom,
-      attributionControl: false,
-      zoomControl: false,
-      zoom: 4,
-      zoomSnap: 0.5,
-      zoomDelta: 0.5,
-      wheelPxPerZoomLevel: 120,
-    });
+    import('leaflet').then(async (leaflet) => {
+      // @ts-ignore
+      await import('tilelayer-canvas');
 
-    leafletMap.fitBounds(latLngBounds, { animate: false });
+      const WorldTiles = (map: string): new () => TileLayer =>
+        // @ts-ignore
+        leaflet.TileLayer.Canvas.extend({
+          // options: {
+          //   errorTileUrl: `${VITE_API_ENDPOINT}/assets/map/empty.webp`,
+          // },
+          getTileUrl(coords: { x: number; y: number; z: number }) {
+            const zoom = 8 - coords.z - 1;
+            const multiplicators = [1, 2, 4, 8, 16, 32, 64];
+            const x = coords.x * multiplicators[zoom - 1];
+            const y = (-coords.y - 1) * multiplicators[zoom - 1];
+            if (x < 0 || y < 0 || y >= 64 || x >= 64) {
+              return `https://aeternum-map.gg/assets/map/empty.webp`;
+            }
+            return `https://aeternum-map.gg/assets/${map}/map_l${zoom}_y${toThreeDigits(
+              y
+            )}_x${toThreeDigits(x)}.webp`;
+          },
+          getTileSize() {
+            return { x: 1024, y: 1024 };
+          },
+        });
 
-    const worldTiles = new (WorldTiles(AETERNUM_MAP.folder))();
-    worldTiles.addTo(leafletMap);
-    console.log(data);
-    data.forEach(({ position }) => {
-      const circle = leaflet.circle(
-        [position.location[0], position.location[1]],
-        { color: 'white' }
-      );
-      circle.addTo(leafletMap!);
+      const worldCRS = leaflet.extend({}, leaflet.CRS.Simple, {
+        transformation: new leaflet.Transformation(1 / 16, 0, -1 / 16, 0),
+      });
+
+      const latLngBounds = leaflet.latLngBounds(AETERNUM_MAP.maxBounds);
+      leafletMap = leaflet.map(mapElement, {
+        preferCanvas: true,
+        crs: worldCRS,
+        maxZoom: AETERNUM_MAP.maxZoom,
+        minZoom: AETERNUM_MAP.minZoom,
+        attributionControl: false,
+        zoomControl: false,
+        zoom: 4,
+        zoomSnap: 0.5,
+        zoomDelta: 0.5,
+        wheelPxPerZoomLevel: 120,
+      });
+
+      leafletMap.fitBounds(latLngBounds, { animate: false });
+
+      const worldTiles = new (WorldTiles(AETERNUM_MAP.folder))();
+      worldTiles.addTo(leafletMap);
+      console.log(data);
+      data.forEach(({ position }) => {
+        const circle = leaflet.circle(
+          [position.location[0], position.location[1]],
+          { color: 'white' }
+        );
+        circle.addTo(leafletMap!);
+      });
     });
   }, [data]);
 
