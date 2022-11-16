700f8c9a8e0f9507f531d9833c9696df5cec3d59
lmachens <leon@machens.koeln>
2022-11-16 20:16:03 +0100

    Fix search by category

--- packages/ui/components/MapFilter/searchMapFilter.ts
+++
@@ -1,9 +0,0 @@
-import { escapeRegExp } from '../../utils/regExp';
-import type { FilterItem } from 'static';
-
-export function searchMapFilter(
-  search: string
-): (mapFilter: FilterItem) => boolean {
-  const regExp = new RegExp(escapeRegExp(search), 'i');
-  return (mapFilter: FilterItem) => Boolean(mapFilter.title.match(regExp));
-}
