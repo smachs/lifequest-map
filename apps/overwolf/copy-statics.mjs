ae9110f5dbadee5abca24457359680e86f02dbd9
lmachens <leon@machens.koeln>
2023-03-18 20:26:16 +0100

    Add block_top_window_navigation

--- apps/overwolf/copy-statics.mjs
+++ apps/overwolf/copy-statics.mjs
@@ -5,8 +5,14 @@ import manifest from './manifest.json' assert { type: 'json' };
 manifest.meta.name = manifest.meta.name.replace('-DEV', '');
 delete manifest.data.windows.background.debug_url;
 delete manifest.data.windows.desktop.debug_url;
+delete manifest.data.windows.overlay.debug_url;
 delete manifest.data.windows.minimap.debug_url;
 delete manifest.data.windows.influence.debug_url;
+manifest.data.windows.background.block_top_window_navigation = true;
+manifest.data.windows.desktop.block_top_window_navigation = true;
+manifest.data.windows.overlay.block_top_window_navigation = true;
+manifest.data.windows.minimap.block_top_window_navigation = true;
+manifest.data.windows.influence.block_top_window_navigation = true;
 
 await fs.writeFile('./dist/manifest.json', JSON.stringify(manifest));
 await fs.cp('./icons/', './dist/icons/', { recursive: true });
