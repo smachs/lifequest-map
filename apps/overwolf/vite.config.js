d51a20e1b456f376f42a9eaed965f75c188b9cf2
lmachens <leon@machens.koeln>
2023-04-06 15:44:07 +0200

    Add PWA support

--- apps/overwolf/vite.config.js
+++ apps/overwolf/vite.config.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
--- apps/www/vite.config.js
+++ apps/www/vite.config.js
@@ -1,15 +1,68 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const { PORT = 3001 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'android-chrome-512x512.png',
+      ],
+      manifest: {
+        name: 'Aeternum Map - New World Map',
+        short_name: 'Aeternum Map',
+        description:
+          'Maximize your New World gameplay with Aeternum Map! Discover locations, chests, lore, expeditions & more. Realtime tracking & farming routes. Open-source companion app.',
+        lang: 'en',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'any',
+        dir: 'ltr',
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}'],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     port: 5174,
     proxy: {
@@ -31,7 +84,7 @@ export default defineConfig({
       },
     },
     sourcemap: true,
-    emptyOutDir: true
+    emptyOutDir: true,
   },
   esbuild: {
     jsxInject: `import React from 'react'`,
