deaf697c616d94188d823e8b9a858b7aa902a89f
lmachens <leon@machens.koeln>
2023-04-07 11:15:59 +0200

    Add wakeLock

--- apps/www/src/main.tsx
+++ apps/www/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { initPlausible } from 'ui/utils/stats';
 import './globals.css';
 import router from './router';
+import { initWakelock } from './wakelock';
 
 const root = createRoot(document.querySelector('#root')!);
 root.render(
@@ -13,3 +14,4 @@ root.render(
 );
 
 initPlausible();
+initWakelock();
