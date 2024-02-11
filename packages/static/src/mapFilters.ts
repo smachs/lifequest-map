0229f1ef1cc47450b05d0ed8feefccbd05dadc19
Gabriel Correa <correaoliveiragabriel@gmail.com>
2024-02-10 21:48:38 -0300

    feat!: new markers

--- packages/static/src/mapFilters.ts
+++ packages/static/src/mapFilters.ts
@@ -29,6 +29,27 @@ export const lootableMapFilters = [
 export const mapFilters: FilterItem[] = [
   {
     category: 'places',
+    type: 'hotels',
+    title: 'Hotels',
+    iconUrl: '/pois/hotels.webp',
+    hasName: true,
+  },
+  {
+    category: 'places',
+    type: 'hostels',
+    title: 'Hostels',
+    iconUrl: '/pois/hostels.webp',
+    hasName: true,
+  },
+  {
+    category: 'places',
+    type: 'restaurants',
+    title: 'Restaurants',
+    iconUrl: '/pois/restaurants.webp',
+    hasName: true,
+  },
+  {
+    category: 'places',
     type: 'beach',
     title: 'Beach',
     iconUrl: '/pois/beach.webp',
@@ -36,9 +57,23 @@ export const mapFilters: FilterItem[] = [
   },
   {
     category: 'places',
+    type: 'parks',
+    title: 'Parks',
+    iconUrl: '/pois/parks.webp',
+    hasName: true,
+  },
+  {
+    category: 'places',
     type: 'nationalPark',
     title: 'National Park',
-    iconUrl: '/pois/national-park.webp',
+    iconUrl: '/pois/nationalpark.webp',
+    hasName: true,
+  },
+  {
+    category: 'places',
+    type: 'mountains',
+    title: 'Mountains',
+    iconUrl: '/pois/mountains.webp',
     hasName: true,
   },
   {
@@ -61,7 +96,101 @@ export const mapFilters: FilterItem[] = [
     title: 'Stargazing',
     iconUrl: '/pois/stargazing.webp',
     hasName: true,
-  },  
+  },
+
+  {
+    category: 'work',
+    type: 'meetup',
+    title: 'Meetup',
+    iconUrl: '/pois/meetup.webp',
+    hasName: true,
+  },
+  {
+    category: 'work',
+    type: 'hackathon',
+    title: 'Hackathon',
+    iconUrl: '/pois/hackathon.webp',
+    hasName: true,
+  },
+  {
+    category: 'work',
+    type: 'bootcamp',
+    title: 'Bootcamp',
+    iconUrl: '/pois/bootcamp.webp',
+    hasName: true,
+  },
+  {
+    category: 'work',
+    type: 'coworking',
+    title: 'Coworking',
+    iconUrl: '/pois/coworking.webp',
+    hasName: true,
+  },
+
+  {
+    category: 'social',
+    type: 'network',
+    title: 'Network',
+    iconUrl: '/pois/network.webp',
+    hasName: true,
+  },
+  {
+    category: 'social',
+    type: 'party',
+    title: 'Party',
+    iconUrl: '/pois/party.webp',
+    hasName: true,
+  },
+  {
+    category: 'social',
+    type: 'afterParty',
+    title: 'After Party',
+    iconUrl: '/pois/afterparty.webp',
+    hasName: true,
+  },
+
+  {
+    category: 'activities',
+    type: 'gym',
+    title: 'Gym',
+    iconUrl: '/pois/gym.webp',
+    hasName: true,
+  },
+  {
+    category: 'activities',
+    type: 'running',
+    title: 'Running',
+    iconUrl: '/pois/running.webp',
+    hasName: true,
+  },
+  {
+    category: 'activities',
+    type: 'skateparks',
+    title: 'Skateparks',
+    iconUrl: '/pois/skateparks.webp',
+    hasName: true,
+  },
+  {
+    category: 'activities',
+    type: 'swinning',
+    title: 'Swinning',
+    iconUrl: '/pois/swinning.webp',
+    hasName: true,
+  },
+  {
+    category: 'activities',
+    type: 'surf',
+    title: 'Surf',
+    iconUrl: '/pois/surf.webp',
+    hasName: true,
+  },
+  {
+    category: 'activities',
+    type: 'fishing',
+    title: 'Fishing',
+    iconUrl: '/pois/fishing.webp',
+    hasName: true,
+  },
 ];
 
 export type MapFiltersCategory = {
@@ -76,6 +205,24 @@ export const mapFiltersCategories: MapFiltersCategory[] = [
     value: 'places',
     title: 'Places',
     filters: mapFilters.filter((mapFilter) => mapFilter.category === 'places'),
+    borderColor: 'rgba(246, 177, 66, 0.7)',
+  },
+  {
+    value: 'social',
+    title: 'Social',
+    filters: mapFilters.filter((mapFilter) => mapFilter.category === 'social'),
+    borderColor: 'rgba(246, 177, 66, 0.7)',
+  },
+  {
+    value: 'activities',
+    title: 'Activities',
+    filters: mapFilters.filter((mapFilter) => mapFilter.category === 'activities'),
+    borderColor: 'rgba(246, 177, 66, 0.7)',
+  },
+  {
+    value: 'work',
+    title: 'Work',
+    filters: mapFilters.filter((mapFilter) => mapFilter.category === 'work'),
     borderColor: 'rgba(246, 177, 66, 0.7)',
   },
 ];
